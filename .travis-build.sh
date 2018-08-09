33d79b2ddd63078f4f58ac4c91e2e02ca69e8b2a
Michael Ernst <mernst@cs.washington.edu>
2018-08-09 13:31:44 -0700

    Use appropriate fork, and respect environment variables

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Entering checker-framework-demos/.travis-build.sh"
+
 # Fail the whole script if any command fails
 set -e
 
@@ -7,11 +9,29 @@ export SHELLOPTS
 
 ROOT=$TRAVIS_BUILD_DIR/..
 
+export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-$ROOT/checker-framework}`
+
 ## Build Checker Framework
-(cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
-# This also builds jsr308-langtools
-(cd $ROOT/checker-framework && ./.travis-build-without-test.sh downloadjdk)
-export CHECKERFRAMEWORK=$ROOT/checker-framework
+SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+if [[ "$SLUGOWNER" == "" ]]; then
+  SLUGOWNER=typetools
+fi
+
+## Build Checker Framework
+if [ -d $CHECKERFRAMEWORK ] ; then
+    git -C $CHECKERFRAMEWORK pull
+else
+    [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
+    REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
+    BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
+    echo "About to execute: (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)"
+    (cd $CHECKERFRAMEWORK/.. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
+fi
+
+# This also builds annotation-tools and jsr308-langtools
+(cd ../checker-framework/ && ./.travis-build-without-test.sh downloadjdk jdk8)
 
 ## Run test
 ant -Djsr308.home=$ROOT
+
+echo "Exiting checker-framework-demos/.travis-build.sh"
