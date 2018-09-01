c5bf2aa3ad0b116fda02ed7981fab6c01b59542a
Michael Ernst <mernst@cs.washington.edu>
2018-09-01 04:35:57 -0700

    Give path to git clone rather than using cd

--- .travis-build.sh
+++ .travis-build.sh
@@ -24,8 +24,8 @@ else
     [ -d /tmp/plume-scripts ] || (cd /tmp && git clone --depth 1 https://github.com/plume-lib/plume-scripts.git)
     REPO=`/tmp/plume-scripts/git-find-fork ${SLUGOWNER} typetools checker-framework`
     BRANCH=`/tmp/plume-scripts/git-find-branch ${REPO} ${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}`
-    echo "About to execute: (cd .. && git clone -b $BRANCH --single-branch --depth 1 $REPO)"
-    (cd $CHECKERFRAMEWORK/.. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}) || (cd .. && git clone -b ${BRANCH} --single-branch --depth 1 ${REPO})
+    echo "About to execute: (git clone -b $BRANCH --single-branch --depth 1 $REPO)"
+    git clone -b ${BRANCH} --single-branch --depth 1 ${REPO}{CHECKERFRAMEWORK} || git clone -b ${BRANCH} --single-branch --depth 1 ${REPO} ${CHECKERFRAMEWORK}
 fi
 
 # This also builds annotation-tools and jsr308-langtools
