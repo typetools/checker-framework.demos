6d0fc19975ac3e90ee14389f0b2bc5e76a4e836d
Michael Ernst <mernst@alum.mit.edu>
2019-09-07 11:00:09 -0700

    Update use of git-clone-related

--- .travis-build.sh
+++ .travis-build.sh
@@ -15,12 +15,7 @@ git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
 
 ## Build Checker Framework
-if [ -d $CHECKERFRAMEWORK ] ; then
-    # Fails if not currently on a branch
-    git -C $CHECKERFRAMEWORK pull || true
-else
-    /tmp/plume-scripts/git-clone-related typetools checker-framework
-fi
+/tmp/plume-scripts/git-clone-related typetools checker-framework ${CHECKERFRAMEWORK}
 
 # This also builds annotation-tools and jsr308-langtools
 (cd $CHECKERFRAMEWORK && ./.travis-build-without-test.sh downloadjdk jdk8)
