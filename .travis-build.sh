917c4b84d5ba40715291747493e48475df90dd20
Michael Ernst <mernst@alum.mit.edu>
2019-10-12 13:23:23 -0700

    Use existing checker-framework directory if available

--- .travis-build.sh
+++ .travis-build.sh
@@ -15,7 +15,12 @@ git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
 
 ## Build Checker Framework
-/tmp/plume-scripts/git-clone-related typetools checker-framework ${CHECKERFRAMEWORK}
+if [ -d $CHECKERFRAMEWORK ] ; then
+    # Fails if not currently on a branch
+    git -C $CHECKERFRAMEWORK pull || true
+else
+    /tmp/plume-scripts/git-clone-related typetools checker-framework ${CHECKERFRAMEWORK}
+fi
 
 # This also builds annotation-tools and jsr308-langtools
 (cd $CHECKERFRAMEWORK && ./.travis-build-without-test.sh downloadjdk jdk8)
