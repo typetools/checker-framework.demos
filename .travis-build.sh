4f4671fff0eabd01dbaa00a8240d0420f41b95dc
Michael Ernst <mernst@cs.washington.edu>
2019-12-23 21:27:33 -0800

    When ignoring output, run quietly

--- .travis-build.sh
+++ .travis-build.sh
@@ -12,7 +12,7 @@ ROOT=`readlink -f ..`
 export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-$ROOT/checker-framework}`
 
 if [ -d "/tmp/plume-scripts" ] ; then
-  git -C /tmp/plume-scripts pull > /dev/null 2>&1
+  git -C /tmp/plume-scripts pull -q > /dev/null 2>&1
 else
   git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
 fi
